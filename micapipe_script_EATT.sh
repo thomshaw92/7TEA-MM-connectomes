3156c77fee9cf69c970ae74b0f9764cbf62ff85b
thomshaw92 <thomasbasilshaw@gmail.com>
2023-02-06 22:05:32 +1000

    various script updates

--- micapipe_script_EATT.sh
+++ micapipe_script_EATT.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#run micapipe on 7TEA data
-#TBS 20220917
+#run micapipe on EATT data
+#TBS 20221110
 for subjName in `cat /30days/uqtshaw/EATT/bids/subjnames.csv` ; do
     singularity_image="/data/lfs2/uqtshaw/micapipe-latest.simg"
     fastsurfer_simg="/data/lfs2/uqtshaw/fastsurfer.sif"
-    base_dir="/30days/uqtshaw/7EATT"
+    base_dir="/30days/uqtshaw/EATT"
     #if [[ ! -e ${base_dir}/bids/derivatives/freesurfer/${subjName}_ses-01/scripts/recon-all.done ]] ; then
 #	singularity exec -B ${base_dir}/bids:/data \
 #                    -B ${base_dir}/bids/derivatives/freesurfer/:/output \
@@ -15,11 +15,12 @@ for subjName in `cat /30days/uqtshaw/EATT/bids/subjnames.csv` ; do
 #                    --fs_license /fs_license/license.txt \
 #                    --t1 /data/${subjName}/ses-01/anat/${subjName}_ses-01_T1w.nii.gz \
 #                    --sid ${subjName}_ses-01 --sd /output --surfreg --fsaparc --threads 40 \
-#                    --parallel
-#    fi
-
+    #                    --parallel
+    #    fi
+    cd ${base_dir}
 
     singularity run --cleanenv \
+		-B /30days/:/30days/ \
 		-B ${base_dir}/bids:/bids:ro \
 		-B ${base_dir}/bids/derivatives:/output_directory \
 		-B ${base_dir}/micapipe_work_dir:/tmp \
@@ -32,16 +33,15 @@ for subjName in `cat /30days/uqtshaw/EATT/bids/subjnames.csv` ; do
 		-proc_structural \
 		-post_structural \
 		-MPC \
-		-microstructural_img /bids/${subjName}/ses-01/anat/${subjName}_ses-01_T1map.nii.gz \
-		-microstructural_reg /bids/${subjName}/ses-01/anat/${subjName}_ses-01_IV1.nii.gz \
+		-microstructural_img /30days/uqtshaw/EATT/bids/${subjName}/ses-01/anat/${subjName}_ses-01_T1map.nii.gz \
 		-proc_dwi \
-		-dwi_main /bids/${subjName}/ses-01/dwi/${subjName}_ses-01_acq-b1000_dir-AP_run-1_dwi.nii.gz,/bids/${subjName}/ses-01/dwi/${subjName}_ses-01_acq-b2500_dir-AP_run-1_dwi.nii.gz \
-		-dwi_rpe /bids/${subjName}/ses-01/dwi/${subjName}_ses-01_acq-b0_dir-PA_run-1_dwi.nii.gz \
+		-dwi_main /30days/uqtshaw/EATT/bids/${subjName}/ses-01/dwi/${subjName}_ses-02_acq-APMULTIBANDBLOCK1DIFFUSIONAP30DIR_dir-COL_dwi.nii.gz,/30days/uqtshaw/EATT/bids/${subjName}/ses-01/dwi/${subjName}_ses-02_acq-APMULTIBANDBLOCK2DIFFUSIONAP30DIR_dir-COL_dwi.nii.gz \
+		-dwi_rpe /30days/uqtshaw/EATT/bids/${subjName}/ses-01/dwi/${subjName}_ses-02_acq-PAMULTIBANDBLOCK1DIFFUSION30DIR_dir-COL_dwi.nii.gz,/30days/uqtshaw/EATT/bids/${subjName}/ses-01/dwi/${subjName}_ses-02_acq-PAMULTIBANDBLOCK2DIFFUSION30DIR_dir-COL_dwi.nii.gz \
 		-SC \
 		-tracts 20M \
 		-proc_rsfmri \
 		-mainScanStr rest_run_bold \
-		-fmri_pe /bids/${subjName}/ses-01/func/${subjName}_ses-01_rest_run_bold.nii.gz \
+		-fmri_pe /30days/uqtshaw/EATT/bids/${subjName}/ses-01/func/${subjName}_ses-01_rest_run_bold.nii.gz \
 		-regress_WM_CSF \
 		-noFIX \
 		-regAffine \
