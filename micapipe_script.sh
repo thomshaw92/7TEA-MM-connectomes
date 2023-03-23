88076a84a70844c5de8c0da61237e66bb215e0f2
thomshaw92 <thomasbasilshaw@gmail.com>
2023-03-23 20:47:38 +1000

    updates for syd data

--- micapipe_script.sh
+++ micapipe_script.sh
@@ -2,21 +2,21 @@
 
 #run micapipe on 7TEA data
 #TBS 20220917
-for subjName in `cat /30days/uqtshaw/EATT/bids/subjnames.csv` ; do
+for subjName in sub-080 sub-090 sub-100 sub-110 sub-120 ; do
     singularity_image="/data/lfs2/uqtshaw/micapipe-latest.simg"
     fastsurfer_simg="/data/lfs2/uqtshaw/fastsurfer.sif"
-    base_dir="/30days/uqtshaw/7EATT"
-    #if [[ ! -e ${base_dir}/bids/derivatives/freesurfer/${subjName}_ses-01/scripts/recon-all.done ]] ; then
-#	singularity exec -B ${base_dir}/bids:/data \
-#                    -B ${base_dir}/bids/derivatives/freesurfer/:/output \
-#                    -B /data/lfs2/uqtshaw/:/fs_license \
-#                    ${fastsurfer_simg} \
-#                    /fastsurfer/run_fastsurfer.sh \
-#                    --fs_license /fs_license/license.txt \
-#                    --t1 /data/${subjName}/ses-01/anat/${subjName}_ses-01_T1w.nii.gz \
-#                    --sid ${subjName}_ses-01 --sd /output --surfreg --fsaparc --threads 40 \
-#                    --parallel
-#    fi
+    base_dir="/90days/uqtshaw/USyd_UQ_dwi_examples"
+   if [[ ! -e ${base_dir}/bids/derivatives/freesurfer/${subjName}_ses-01/scripts/recon-all.done ]] ; then
+	singularity exec -B ${base_dir}/bids:/data \
+                    -B ${base_dir}/bids/derivatives/freesurfer/:/output \
+                    -B /data/lfs2/uqtshaw/:/fs_license \
+                    ${fastsurfer_simg} \
+                    /fastsurfer/run_fastsurfer.sh \
+                    --fs_license /fs_license/license.txt \
+                    --t1 /data/${subjName}/anat/${subjName}_T1w.nii.gz \
+                    --sid ${subjName}_ses-01 --sd /output --surfreg --fsaparc --threads 40 \
+                    --parallel
+    fi
 
 
     singularity run --cleanenv \
@@ -31,19 +31,11 @@ for subjName in `cat /30days/uqtshaw/EATT/bids/subjnames.csv` ; do
 		-ses ses-01 \
 		-proc_structural \
 		-post_structural \
-		-MPC \
-		-microstructural_img /bids/${subjName}/ses-01/anat/${subjName}_ses-01_T1map.nii.gz \
-		-microstructural_reg /bids/${subjName}/ses-01/anat/${subjName}_ses-01_IV1.nii.gz \
-		-proc_dwi \
-		-dwi_main /bids/${subjName}/ses-01/dwi/${subjName}_ses-01_acq-b1000_dir-AP_run-1_dwi.nii.gz,/bids/${subjName}/ses-01/dwi/${subjName}_ses-01_acq-b2500_dir-AP_run-1_dwi.nii.gz \
-		-dwi_rpe /bids/${subjName}/ses-01/dwi/${subjName}_ses-01_acq-b0_dir-PA_run-1_dwi.nii.gz \
+		_dwi \
+		-dwi_main /bids/${subjName}/ses-01/dwi/${subjName}_dir-AP_dwi.nii.gz \
+		-dwi_rpe /bids/${subjName}/ses-01/dwi/${subjName}_dir-PA_dwi.nii.gz \
 		-SC \
 		-tracts 20M \
-		-proc_rsfmri \
-		-mainScanStr rest_run_bold \
-		-fmri_pe /bids/${subjName}/ses-01/func/${subjName}_ses-01_rest_run_bold.nii.gz \
-		-regress_WM_CSF \
-		-noFIX \
 		-regAffine \
 		-GD \
 		-Morphology \
@@ -51,4 +43,3 @@ for subjName in `cat /30days/uqtshaw/EATT/bids/subjnames.csv` ; do
 		-QC_subj
 
 done
-
